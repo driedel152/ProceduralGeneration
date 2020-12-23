8b5c5681d1bb7bb3436605c993342ea646c033b9
driedel152 <45524258+driedel152@users.noreply.github.com>
2020-12-23 15:41:03 -0600

    Made a MarchingCubeGenerator
    Generates triangles based on the given active vertices.

---
+++ Assets/Editor/MarchingCubeGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MarchingCubeGenerator))]
+public class MarchingCubeGeneratorEditor : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		MarchingCubeGenerator preview = (MarchingCubeGenerator)target;
+
+		if (DrawDefaultInspector())
+		{
+			if (preview.autoUpdate)
+			{
+				preview.DrawTriangles();
+			}
+		}
+
+		if (GUILayout.Button("Generate"))
+		{
+			preview.DrawTriangles();
+		}
+	}
+}
